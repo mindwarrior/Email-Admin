508511c95abe043a8bdc61bd4c5928726f57bdbd
Kartik <kartik91827@gmail.com>
2019-03-07 16:41:02 -0500

    Added method to set the department.

---
+++ src/emailapp/Email.java
@@ -0,0 +1,41 @@
+
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	String password;
+	String department;
+	int mailboxCapacity;
+	String alternateEmail;
+
+	// constructor to recieve first and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email created:" + this.firstName + " " + this.lastName);
+		// Call a method asking for dept and return the department
+		this.department = setDepartment();
+	}
+
+	// Ask for the department
+	private String setDepartment() {
+		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {
+			return "sales";
+		} else if (depChoice == 2) {
+			return "dev";
+		} else if (depChoice == 3) {
+			return "acct";
+		} else {
+			return "";
+		}
+	}
+	// Set the mailbox capacity
+	// Set alternate email
+	// Change the password
+}
